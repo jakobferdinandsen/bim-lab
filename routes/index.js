cf397c6f4b31324f55da0a9e2d14e5d650c103f3
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-04-07 11:49:03 +0200

    Added no config/no article found to frontpage

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,25 @@ var article = require('../models/article');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     config.findOne({name: 'selectedArticle'}, function (err, config) {
+        if (config === null) {
+            res.render('index', {
+                title: 'BIM Lab',
+                articleHeader: 'Config not found',
+                articleBody: 'Config not found',
+                articleImage: 'https://upload.wikimedia.org/wikipedia/en/timeline/fe8adfad0b276ed3a6e90ce75db72f26.png'
+            });
+            return;
+        }
         article.findOne({_id: config.value}, function (err, article) {
+            if (article === null) {
+                res.render('index', {
+                    title: 'BIM Lab',
+                    articleHeader: 'Article not found',
+                    articleBody: 'Article not found',
+                    articleImage: 'https://upload.wikimedia.org/wikipedia/en/timeline/fe8adfad0b276ed3a6e90ce75db72f26.png'
+                });
+                return;
+            }
             res.render('index', {
                 title: 'BIM Lab',
                 articleHeader: article.header,
